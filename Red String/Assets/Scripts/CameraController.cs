b992f57e540ec0ecf82c4a7d49e3285a94fdda28
michaelw99 <michaelw99@berkeley.edu>
2017-11-03 17:38:02 -0700

    shitcamerascroll

--- Red String/Assets/Scripts/CameraController.cs
+++ Red String/Assets/Scripts/CameraController.cs
@@ -6,13 +6,18 @@ using UnityEngine;
 public class CameraController : MonoBehaviour {
 
 	public GameObject player;
+	public float dampTime;
 
 	private Vector3 offset; 
+	private Camera camera;
+	private Vector3 velocity = Vector3.zero;
 
 	// Use this for initialization
 	void Start () {
 		player = transform.parent.gameObject;
 		offset = transform.position - player.transform.position;
+		camera = gameObject.GetComponent<Camera>();
+		dampTime = 1.0f;
 	}
 	
 	// Update is called once per frame
@@ -23,7 +28,11 @@ public class CameraController : MonoBehaviour {
 	// LateUpdate is called after Update each frame
 	void LateUpdate () 
 	{
+		Vector3 point = camera.WorldToViewportPoint(player.transform.position + offset);
+		Vector3 delta = player.transform.position + offset - camera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z)); //(new Vector3(0.5, 0.5, point.z));
+		Vector3 destination = transform.position + delta;
+		transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
 		// Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
-		transform.position = new Vector3(player.transform.position.x, 0 , 0) + offset;
+//		transform.position = new Vector3(player.transform.position.x, 0 , 0) + offset;
 	}
 }
