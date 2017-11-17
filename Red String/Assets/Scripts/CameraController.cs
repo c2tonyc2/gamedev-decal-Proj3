2b49a4b2865d80ee21b5b764782e27830aaefebb
michaelw99 <michaelw99@berkeley.edu>
2017-11-16 17:36:49 -0800

    stuff and time step change

--- Red String/Assets/Scripts/CameraController.cs
+++ Red String/Assets/Scripts/CameraController.cs
@@ -32,7 +32,7 @@ public class CameraController : MonoBehaviour {
 	void LateUpdate () 
 	{
 		Vector3 point = camera.WorldToViewportPoint(player.transform.position + offset);
-		Vector3 delta = player.transform.position + offset - camera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z)); //(new Vector3(0.5, 0.5, point.z));
+		Vector3 delta = player.transform.position + offset - camera.ViewportToWorldPoint(new Vector3(0.5f, point.y, point.z)); //(new Vector3(0.5, 0.5, point.z));
 		Vector3 destination = transform.position + delta;
 		transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
 		// Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
